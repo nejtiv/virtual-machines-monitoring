874c5cd5e60023ad4470cff54170cb3bac491a49
nejtiv <kaczmarczyk2004inw@gmail.com>
2025-12-08 20:23:20 +0100

    feat:initialized backend structure

---
+++ backend/api/__init__.py
@@ -0,0 +1,9 @@
+#===APP INITIALIZATION
+from flask import Flask
+
+#===INIT APP
+def create_app():
+    app = Flask(__name__)
+
+
+    return app
---
+++ backend/api/config.py
@@ -0,0 +1,8 @@
+#==APP CONFIG
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config(object):
+    SECRET_KEY = os.environ.get("SECRET_KEY")
