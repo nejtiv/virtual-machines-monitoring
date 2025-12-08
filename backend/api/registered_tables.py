154b9a8e47311f788b6dac75c5844e1ec3939c68
nejtiv <kaczmarczyk2004inw@gmail.com>
2025-12-08 22:22:46 +0100

    feat: added datatables registration

---
+++ backend/api/registered_tables.py
@@ -0,0 +1,6 @@
+#===REGISTERED SQL TABLES
+from .db_connection import engine, sa
+
+metadata = sa.MetaData()
+#===REGISTER TABLES HERE
+virtualmachines = sa.Table("VirtualMachines", metadata, autoload_with=engine, schema="dbo")
