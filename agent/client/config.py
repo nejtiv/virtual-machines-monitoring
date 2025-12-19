c23b3d7b7f02614623053d96884cc144491c0f42
nejtiv <kaczmarczyk2004inw@gmail.com>
2025-12-19 23:48:43 +0100

    security: Added secrets

--- agent/client/config.py
+++ agent/client/config.py
@@ -1,2 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config(object):
-    SECRET_KEY = 'e1d3c85ee93403828efba772808ae720771d586fbbea6817d4ce4f5e80b4427a'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
