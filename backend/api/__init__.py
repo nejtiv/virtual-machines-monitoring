a2a0020680741188a9e0af4046dc1933fface692
nejtiv <kaczmarczyk2004inw@gmail.com>
2025-12-26 01:12:25 +0100

    feat: added basic logging

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,11 +1,22 @@
 #===APP INITIALIZATION
 from flask import Flask
 from flask_cors import CORS
+import logging
+from logging.handlers import RotatingFileHandler
 
 #===INIT APP
 def create_app():
     app = Flask(__name__)
     CORS(app)
+
+    #===CONFIGURE LOGGING
+    handler = RotatingFileHandler("app.log", maxBytes=1000000, backupCount=3)
+    handler.setLevel(logging.WARNING)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
 
     #===REGISTER BLUEPRINTS
     from .routes.crud_vmping import vm
