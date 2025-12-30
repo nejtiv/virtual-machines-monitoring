84dba01ad083fb24fb3bbff7d0539917a3935852
nejtiv <kaczmarczyk2004inw@gmail.com>
2025-12-30 19:14:52 +0100

    feat: refreshers rewritten in scheduled jobs

--- backend/api/routes/routes_hardwareinfo.py
+++ backend/api/routes/routes_hardwareinfo.py
@@ -38,39 +38,3 @@ def connection_check():
         return jsonify({"message": results}), 200
     except Exception as e:
         return jsonify({"error": f"An error occured during checking if the agent is online. Details: {e}"}), 500
-
-@hardware_check.route("/check", methods=["PUT"])
-def check():
-    #list for vms status to append
-    results = []
-    try:
-        select_query = sa.select(hardwareinfo.c.ipv4, hardwareinfo.c.vm_id)
-        with engine.begin() as connection:
-            result = connection.execute(select_query).fetchall()
-        for vm in result:
-            ipv4 = vm.ipv4
-            vm_id = vm.vm_id
-            try:
-                #Call the function to bulk hardware parameters check
-                check_info = check_hardware(ipv4 = ipv4)
-                dto = hardwareupdate(**check_info)
-                #Query to update the status of current machine monitored
-                update_query = sa.update(hardwareinfo).where(hardwareinfo.c.vm_id == vm_id).values(**dto.model_dump())
-                with engine.begin() as connection:
-                    connection.execute(update_query)
-                results.append (
-                    {
-                        "vm_id": vm_id,
-                        "status": "Success!"
-                    }
-                )
-            except Exception as e:
-                results.append(
-                    {
-                        "vm_id": vm_id,
-                        "status": "Failed!"
-                    }
-                )
-        return jsonify({"message": results}), 200
-    except Exception as e:
-        return jsonify({"error": f"An error occured during checking hardware info of Virtual Machines. Details: {e}"}), 500
--- backend/api/tasks/jobs.py
+++ backend/api/tasks/jobs.py
@@ -1,8 +1,50 @@
 #===REGISTER BACKGROUND JOBS
 from .. import scheduler
+from ..db_connection import sa, engine
+from ..models.models_hardwareinfo import hardwareupdate
+from ..registered_tables import hardwareinfo
 from ..services.vm_ping import vm_ping
+from ..services.hardware_check import check_hardware
 
 #Virtual Machine Online/Offline check job
 @scheduler.task(trigger='interval', id='vm_ping', seconds=1)
 def vm_ping_job():
     vm_ping()
+
+#Check hardware parameters of each Virtual Machine job
+@scheduler.task(trigger='interval', id='hardware_check', seconds=2)
+def hardware_check_job():
+    #List of Virtual Machines to append
+    results = []
+    try:
+        #Query to select all registered Virtual Machines for monitoring
+        select_query = sa.select(hardwareinfo.c.ipv4, hardwareinfo.c.vm_id)
+        with engine.begin() as connection:
+            result = connection.execute(select_query).fetchall()
+        for vm in result:
+            vm_id = vm.vm_id
+            ipv4 = vm.ipv4
+            try:
+                #Call the service to check the hardware for each vm
+                check_info = check_hardware(ipv4= ipv4)
+                dto = hardwareupdate(**check_info)
+                #Query to update the hardware info for Virtual Machine
+                update_query = sa.update(hardwareinfo).where(hardwareinfo.c.vm_id == vm_id).values(**dto.model_dump())
+                with engine.begin() as connection:
+                    connection.execute(update_query)
+                results.append(
+                    {
+                        "vm_id": vm_id,
+                        "status": "success!"
+                    }
+                )
+            except Exception as e:
+                results.append(
+                    {
+                        "vm_id": vm_id,
+                        "status": "failed!"
+                    }
+                )
+            return results
+    except Exception as e: 
+        return e
