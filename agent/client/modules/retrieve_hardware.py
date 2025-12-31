5e630247ec9f2b660e8b9a2cb477248fe0b222b1
nejtiv <kaczmarczyk2004inw@gmail.com>
2025-12-31 01:38:32 +0100

    fix: fixed execution of hardware check

--- agent/client/modules/retrieve_hardware.py
+++ agent/client/modules/retrieve_hardware.py
@@ -1,10 +1,6 @@
 #===hardware monitoring for vm
 import psutil
 
-memory = psutil.virtual_memory()
-disks = psutil.disk_usage('/')
-usr = psutil.users()
-
 def get_size(bytes, suffix="B"):
     factor = 1024
     for unit in ["", "K", "M", "G", "T", "P"]:
@@ -13,12 +9,15 @@ def get_size(bytes, suffix="B"):
         bytes /= factor
 
 def get_hardware():
+    memory = psutil.virtual_memory()
+    disks = psutil.disk_usage('/')
+    usr = psutil.users()
     info = {
         "cpu_used": psutil.cpu_percent(1),
         "memory_total": get_size(memory.total),
         "memory_used": get_size(memory.used),
         "memory_free": get_size(memory.available),
-        "memory_used_percent" : memory.percent(),
+        "memory_used_percent" : memory.percent,
         "disk_total_size": get_size(disks.total),
         "disk_used": get_size(disks.used),
         "disk_used_percent": disks.percent,
